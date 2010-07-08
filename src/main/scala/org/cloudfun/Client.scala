7c62be278e068cd606c717c021a469337aaa408e
zzorn <zzorn@iki.fi>
2010-07-09 01:41:22 +0300

    Started sketching out a usage example

---
+++ src/main/scala/org/cloudfun/Client.scala
@@ -0,0 +1,11 @@
+package org.cloudfun
+
+/**
+ * CloudFun client-side interface.
+ */
+trait Client {
+
+  
+
+
+}
---
+++ src/test/scala/org/cloudfun/CloudFunExample.scala
@@ -0,0 +1,21 @@
+package org.cloudfun
+
+/**
+ * Usage example.
+ */
+object CloudFunExample {
+  def main(args: Array[String]) {
+
+    // Create local server
+
+    // Create client
+
+    // Connect to the local server
+
+    // Create another client
+
+    // Connect it to a remote server
+
+
+  }
+}
